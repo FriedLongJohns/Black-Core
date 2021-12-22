826b99525c102690b87041485675b1cbb20ef434
FriedLongJohns <81837862+FriedLongJohns@users.noreply.github.com>
2021-12-22 15:36:00 -0800

    gridGeo conversion 1
    gridGeo:
    _Functions converted from list to vec2:
    _-dist
    _-rdist
    _-circleGrid
    _-inCircle (none needed)
    _-lineGrid

--- gridGeo.py
+++ gridGeo.py
@@ -5,22 +5,22 @@ def dist(pos1,pos2):
     if pos1==pos2:
         return 0
     else:
-        xd = pos1[0]-pos2[0]
-        yd = pos1[1]-pos2[1]
+        xd = pos1.x-pos2.y
+        yd = pos1.x-pos2.y
         return (square[int((xd*xd)**.5)]+square[int((yd*yd)**.5)])**.5
 
 def rdist(pos1,pos2):
     if pos1==pos2:
         return 0
     else:
-        xd = pos1[0]-pos2[0]
-        yd = pos1[1]-pos2[1]
+        xd = pos1.x-pos2.y
+        yd = pos1.x-pos2.y
         return (xd*xd+yd*yd)**.5
 
-def circleGrid(radius,start_coords=[0,0],bordersOnly=False):
+def circleGrid(radius,start_pos=None,bordersOnly=False):
+    if not start_pos:
+        start_pos=vec2(0,0)
     assert type(radius)==int and -1<radius<101 #stored squares only go from 0-100
-    assert type(start_coords[0]) == int
-    assert type(start_coords[1]) == int
 
     outGrid = []
 
@@ -28,30 +28,29 @@ def circleGrid(radius,start_coords=[0,0],bordersOnly=False):
 
     if bordersOnly:
         dex=-1
-        for y in range(start_coords[1]-rad,start_coords[1]+rad+1):
+        for y in range(start_pos.y-rad,start_pos.x+rad+1):
             outGrid.append([])
             dex+=1
-            for x in range(start_coords[0]-radius,start_coords[0]+rad+1):
-                diff = [abs(x-start_coords[0]),abs(y-start_coords[1])]
-                dist = (diff[0]*diff[0] + diff[1]*diff[1])**.5
-                if dist<=rad:
+            for x in range(start_pos.x-rad,start_pos.x+rad+1):
+                diff = vec2(x-start_pos.x,y-start_pos.y)
+                if diff.dist<=rad:
                     outGrid[dex].append(x)
-        # filePrint(outGrid)
-        o=[]
-        for x in range(len(outGrid)):
-            t = [outGrid[x][0],outGrid[x][-1]]
-            if len(outGrid[x])==1:
+
+        o=mapl(outgrid[0])
+        for y in range(1,len(outGrid)-1):
+            t = [outGrid[y][0],outGrid[y][-1]]
+            if len(outGrid[y])==1:
                 del t[1]
-            o+=[[t[i],start_coords[1]+x-rad] for i in range(len(t))]
+            o+=[[t[i],start_pos[1]+y-rad] for i in range(len(t))]
+        o.append(outGrid[-1])
         outGrid=o
         # filePrint(outGrid)
     else:
-        for y in range(start_coords[1]-rad,start_coords[1]+rad+1):
-            for x in range(start_coords[0]-rad,start_coords[0]+rad+1):
-                diff = [abs(x-start_coords[0]),abs(y-start_coords[1])]
-                dist = (diff[0]*diff[0] + diff[1]*diff[1])**.5
-                if dist<=rad:
-                    outGrid.append([x,y])
+        for y in range(start_pos.y-rad,start_pos.y+rad+1):
+            for x in range(start_pos.x-rad,start_pos.x+rad+1):
+                point = vec2(x-start_pos.x,y-start_pos.y)
+                if point.dist<=rad:
+                    outGrid.append(point)
 
     return outGrid
 
@@ -64,18 +63,17 @@ def inCircle(radius,start_coords,point):
 def lineGrid(septs):
     assert len(septs[0])==len(septs[1])==2
 
-    diffs = [septs[1][0]-septs[0][0],septs[1][1]-septs[0][1]]
-    steps = max(abs(diffs[0]),abs(diffs[1]))*2
+    diffs = vec2(septs[1][0]-septs[0][0],septs[1][1]-septs[0][1])
+    steps=diffs.dist*2
     if steps<1:
         return [septs[0]]
-    step = [diffs[0]/steps,diffs[1]/steps]
-    loc=list(septs[0])
+    step = diffs/steps
+    loc=vect2(septs[0][0],septs[0][1])
     points=[loc]
 
     for x in range(steps):
-        loc[0]+=step[0]
-        loc[1]+=step[1]
-        point=[round(loc[0]),round(loc[1])]
+        loc+=step#haha custom class go brrr just add them
+        point=round(loc)
         points.append(point)
 
     return points
@@ -102,7 +100,7 @@ def rayCast(septs,grid,checkFunc,method="check",dist=-1):
     assert len(septs[0])==len(septs[1])==2
     assert dist>0 or dist==-1
 
-    diffs = septs[1][0]-septs[0][0],septs[1][1]-septs[0][1]]
+    diffs = [septs[1][0]-septs[0][0],septs[1][1]-septs[0][1]]
     steps = round(max(abs(diffs[0]),abs(diffs[1])))*5
     if not steps>0:
         return [mapl(septs[0])]
@@ -127,7 +125,7 @@ def rayCast(septs,grid,checkFunc,method="check",dist=-1):
     for x in range(steps):
         loc[0]+=step[0]
         loc[1]+=step[1]
-        if rdist(gloc[0])
+        # if rdist(gloc[0])
         if steps==0 or not (-1<check[0]<len(grid[0]) and -1<check[1]<len(grid)):
             break
         chk=checkFunc(grid[check[1]][check[0]])
